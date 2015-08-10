a0dbd7ba961c25504ce484675b9406366b5f8760
yangzihao <572091175@qq.com>
2015-08-10 19:04:09 +0800

    add v1

---
+++ Exam20150810/yangzihao/Exam2_ClientSocket/JFrameWindow.java
@@ -0,0 +1,104 @@
+package com.hand.Exam2_ClientSocket;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class JFrameWindow extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7758767011211561497L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JButton btnNewButton;
+	private JLabel label;
+	private JLabel label_state;
+	private JButton button;
+
+	/**
+	 * Launch the application.
+	 */
+
+	/**
+	 * Create the frame.
+	 */
+	public JFrameWindow() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setText("127.0.0.1");
+		
+		btnNewButton = new JButton("链接");
+		
+		label = new JLabel("当前链接状态：");
+		
+		label_state = new JLabel("未连接");
+		
+		button = new JButton("下载文件");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(textField, GroupLayout.PREFERRED_SIZE, 286, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(label)
+							.addGap(59)
+							.addComponent(label_state)
+							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(button)))
+					.addContainerGap(24, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnNewButton)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(46)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(label)
+						.addComponent(label_state)
+						.addComponent(button))
+					.addContainerGap(141, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
+		
+		
+		
+///////////////////////////////////////////////////////////////////////////////////////////
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				Action action=new Action();
+				action.connection(textField.getText());
+				
+			}
+		});
+	}
+	
+	
+	public void setState(String state){
+		label_state.setText(state);
+	}
+}
